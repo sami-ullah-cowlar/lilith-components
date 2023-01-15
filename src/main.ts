e6a4f1e4ad748f47958dacca980441486da80451
sami-ullah-cowlar <sami.ullah.cowlar@gmail.com>
2023-01-15 19:44:52 +0500

    import positioning changed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
+import '@/assets/main.css'
 
 export { default as HelloWorld } from "@/components/HelloWorld.vue";
 export { default as TheWelcome } from "@/components/TheWelcome.vue";
 export { default as WelcomeItem } from "@/components/WelcomeItem.vue";
-import '@/assets/main.css'
-
