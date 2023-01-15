e93c95414b9b77b3ff8f859b7e09871456d41602
sami-ullah-cowlar <sami.ullah.cowlar@gmail.com>
2023-01-15 19:55:27 +0500

    Greeter Component added for testing

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,4 @@ import "@/assets/main.css";
 export { default as HelloWorld } from "@/components/HelloWorld.vue";
 export { default as TheWelcome } from "@/components/TheWelcome.vue";
 export { default as WelcomeItem } from "@/components/WelcomeItem.vue";
+export { default as Greeter } from "@/components/Greeter.vue";
